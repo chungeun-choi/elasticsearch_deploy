bcb6da4a41812244d759cd1e7f89beae07cd8d74
cucuridas@gamil.com <3310223@naver.com>
2023-10-05 17:27:52 +0900

    fixed source code

--- deploy.py
+++ deploy.py
@@ -4,19 +4,18 @@ import sys
 
 def run_docker_compose(file_path):
     try:
-        result = subprocess.run(["docker","compose", "-f", file_path, "up", "-d"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run(["docker-compose", "-f", file_path, "up", "-d"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print(result.stdout.decode())
     except subprocess.CalledProcessError as e:
         print(f"Error occurred: {e}")
         print(e.stderr.decode())
 
 def run_all_yml_in_directory(directory_path):
-    for root, dirs, files in os.walk(directory_path):
-        for file in files:
-            if file.endswith(".yml"):
-                yml_path = os.path.join(root, file)
-                print(f"Running docker-compose for: {yml_path}")
-                run_docker_compose(yml_path)
+    for entry in os.scandir(directory_path):
+        if entry.is_file() and entry.name.endswith(".yml"):
+            yml_path = entry.path
+            print(f"Running docker-compose for: {yml_path}")
+            run_docker_compose(yml_path)
 
 # 실행
 #directory_path = sys.argv[1]
