ab5306775a193b1c7fab94bb0c7f96b1db61fb09
cucuridas@gamil.com <3310223@naver.com>
2023-10-05 17:15:59 +0900

    fiexd directory struct

--- deploy.py
+++ deploy.py
@@ -2,10 +2,9 @@ import subprocess
 import os
 import sys
 
-
 def run_docker_compose(file_path):
     try:
-        result = subprocess.run(["docker","compose", "-f", file_path, "up", "-d"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run(["docker-compose", "-f", file_path, "up", "-d"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print(result.stdout.decode())
     except subprocess.CalledProcessError as e:
         print(f"Error occurred: {e}")
@@ -20,5 +19,6 @@ def run_all_yml_in_directory(directory_path):
                 run_docker_compose(yml_path)
 
 # 실행
-directory_path = sys.argv[1]
+#directory_path = sys.argv[1]
+directory_path = "docker/single"
 run_all_yml_in_directory(directory_path)
